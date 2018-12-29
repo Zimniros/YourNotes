2bba4a788e815af20480e75af997b3d6fb805f19
Zimniros <zimniros@protonmail.com>
2018-12-29 14:11:36 +0300

    Add Icons and styles for Sidebar menu.

--- src/renderer/components/Sidebar.js
+++ src/renderer/components/Sidebar.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
+import Icon from 'react-icons-kit';
+import { book } from 'react-icons-kit/fa/book';
+import { star } from 'react-icons-kit/fa/star';
+import { trashO } from 'react-icons-kit/fa/trashO';
+
 const Sidebar = () => (
   <div className="sidebar">
-    <div className="sidebar__main-shortcuts">
-      <ul className="main-shortcuts__list">
-        <li className="main-shortcuts__item">All Notes</li>
-        <li className="main-shortcuts__item">Starred</li>
-        <li className="main-shortcuts__item">Trash</li>
-      </ul>
+    <div className="sidebar__menu">
+      <button type="button" className="menu__item">
+        <Icon className="menu__icon" size="100%" icon={book} />
+        <span className="menu__text">All Notes</span>
+        <span className="menu__count">0</span>
+      </button>
+      <button type="button" className="menu__item">
+        <Icon className="menu__icon" size="100%" icon={star} />
+        <span className="menu__text">Starred</span>
+        <span className="menu__count">0</span>
+      </button>
+      <button type="button" className="menu__item">
+        <Icon className="menu__icon" size="100%" icon={trashO} />
+        <span className="menu__text">Trash</span>
+        <span className="menu__count">0</span>
+      </button>
     </div>
+
     <div className="sidebar__folder-shortcuts">
       <div className="folder-shortcuts__title">Folders</div>
       <ul className="folder-shortcuts__list">
