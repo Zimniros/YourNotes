5ab837e9c7064b389521b5666a48f8bc6833564a
Zimniros <zimniros@protonmail.com>
2019-01-14 21:01:59 +0300

    Add redux and react-redux packages.

--- src/renderer/components/App.js
+++ src/renderer/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Sidebar from './Sidebar/index';
+import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 import NoteList from './NoteList';
 import Details from './Details';
