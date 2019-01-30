434f058af76bb528e1d2728f4330a8d56bbe45b5
Zimniros <zimniros@protonmail.com>
2019-01-30 17:41:08 +0300

    Add styles for NoteItem component.

--- src/renderer/components/NotesViewer/NoteItem.js
+++ src/renderer/components/NotesViewer/NoteItem.js
@@ -4,18 +4,15 @@ import { formatUpdatedAt } from '../lib/timeHelpers';
 import { noteType } from '../../types';
 
 const NoteItem = ({ note }) => {
-  const {
-    title, content, tags, updatedAt,
-  } = note;
+  const { title, content, updatedAt } = note;
 
   return (
     <div className="note-item">
-      <div className="note-item__row">
-        <div className="note-item__title">{title || 'Untitled'}</div>
-        <div className="note-item__updated-at">{formatUpdatedAt(updatedAt)}</div>
+      <div className="note-item__title">{title || 'Untitled'}</div>
+      <div className="note-item__updated-at">{formatUpdatedAt(updatedAt)}</div>
+      <div className="note-item__content">
+        <span>{content || 'Empty note'}</span>
       </div>
-      <div className="note-item__content">{content || 'Empty note'}</div>
-      <div className="note-item__tags">{tags || 'No tags'}</div>
     </div>
   );
 };
