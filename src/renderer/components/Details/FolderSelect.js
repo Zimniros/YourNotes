a16778283a95fdfb78ade2721bc680037bd82fb6
Zimniros <zimniros@protonmail.com>
2019-03-07 18:14:53 +0300

    Add FolderSelect dropdown and styles.

---
+++ src/renderer/components/Details/FolderSelect.js
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { Component } from 'react';
+import { func, instanceOf } from 'prop-types';
+import Icon from '@mdi/react';
+import { mdiFolder as folderIcon } from '@mdi/js';
+
+import Map from '../../../lib/Map';
+
+class FolderSelect extends Component {
+  static propTypes = {
+    onClose: func.isRequired,
+    folders: instanceOf(Map).isRequired,
+  };
+
+  constructor() {
+    super();
+
+    this.selectRef = React.createRef();
+
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleOutsideClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleOutsideClick);
+  }
+
+  handleOutsideClick(event) {
+    const { onClose } = this.props;
+
+    if (!this.selectRef.current.contains(event.target)) {
+      onClose();
+    }
+  }
+
+  render() {
+    const { folders } = this.props;
+
+    const folderList = folders
+      && folders.size
+      && folders.map(({ id, name }) => (
+        <div key={id} className="folder-select__item">
+          <Icon className="folder-select__icon" path={folderIcon} />
+          <span className="folder-select__name">{name}</span>
+        </div>
+      ));
+
+    return folderList ? (
+      <div onClick={e => e.stopPropagation()} ref={this.selectRef} className="folder-select">
+        {folderList}
+      </div>
+    ) : null;
+  }
+}
+
+export default FolderSelect;
--- src/renderer/components/Details/StorageInfoBar.js
+++ src/renderer/components/Details/StorageInfoBar.js
@@ -1,34 +1,65 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { string, instanceOf } from 'prop-types';
 import Icon from '@mdi/react';
 import { mdiFolder as folderIcon } from '@mdi/js';
 
+import FolderSelect from './FolderSelect';
+
 import Map from '../../../lib/Map';
 
-const StorageInfoBar = ({ folderId, folders }) => {
-  const folder = folders.get(folderId);
-  const folderName = folder ? folder.name : 'All Notes';
+class StorageInfoBar extends Component {
+  static propTypes = {
+    folderId: string.isRequired,
+    folders: instanceOf(Map).isRequired,
+  };
 
-  console.log('folderId', folderId);
-  console.log('folders', folders);
-  console.log('folder', folder);
+  constructor() {
+    super();
 
-  return (
-    <div className="title-bar__storage-info storage-info">
-      <div className="storage-info__folder">
-        <Icon className="storage-info__icon" path={folderIcon} />
-        <span className="storage-info__name">{folderName}</span>
+    this.state = {
+      isFolderSelectOpen: false,
+    };
+
+    this.onClose = this.onClose.bind(this);
+    this.getFolderParentSelector = this.getFolderParentSelector.bind(this);
+  }
+
+  onFolderClick() {
+    const { isFolderSelectOpen } = this.state;
+    this.setState({ isFolderSelectOpen: !isFolderSelectOpen });
+  }
+
+  onClose() {
+    this.setState({ isFolderSelectOpen: false });
+  }
+
+  getFolderParentSelector() {
+    return this.folderInfoRef.current;
+  }
+
+  render() {
+    const { isFolderSelectOpen } = this.state;
+    const { folders, folderId } = this.props;
+
+    const folder = folders.get(folderId);
+    const folderName = folder ? folder.name : 'All Notes';
+
+    return (
+      <div className="title-bar__storage-info storage-info">
+        <div className="storage-info__folder" onClick={() => this.onFolderClick()}>
+          <Icon className="storage-info__icon" path={folderIcon} />
+          <span className="storage-info__name">{folderName}</span>
+
+          {isFolderSelectOpen && <FolderSelect onClose={this.onClose} folders={folders} />}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = state => ({ folders: state.folders });
 
 export default connect(mapStateToProps)(StorageInfoBar);
-
-StorageInfoBar.propTypes = {
-  folderId: string.isRequired,
-  folders: instanceOf(Map).isRequired,
-};
