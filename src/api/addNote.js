d0670ffc251ea96f4e8faad136e865502d41868e
Zimniros <zimniros@protonmail.com>
2019-03-20 17:36:14 +0300

    Refactor addNote method for adding notes to folders or tags.

--- src/api/addNote.js
+++ src/api/addNote.js
@@ -1,23 +1,28 @@
 import v4 from 'uuid/v4';
 import db from './db';
 
-function addNote(type, locationId) {
-  const { notes } = db;
-  // const { folders, tags } = storageData;
+async function addNote(type, locationId) {
+  const { notes, folders, tags } = db;
 
-  // if (type && locationId) {
-  //   if (type === 'folder') {
-  //     if (!folders.some(({ id }) => id === locationId)) {
-  //       return Promise.reject(new Error(`A folder with the id '${locationId}' doesn't exist.`));
-  //     }
-  //   }
+  if (type && locationId) {
+    if (type === 'folder') {
+      const targetFolder = folders.findOne({ id: locationId });
+      if (!targetFolder) {
+        return Promise.reject(
+          new Error(`A folder with the id '${locationId}' doesn't exist.`)
+        );
+      }
+    }
 
-  //   if (type === 'tag') {
-  //     if (!tags.some(({ id }) => id === locationId)) {
-  //       return Promise.reject(new Error(`A tag with the id '${locationId}' doesn't exist.`));
-  //     }
-  //   }
-  // }
+    if (type === 'tag') {
+      const targetTag = tags.findOne({ id: locationId });
+      if (!targetTag) {
+        return Promise.reject(
+          new Error(`A tag with the id '${locationId}' doesn't exist.`)
+        );
+      }
+    }
+  }
 
   const newNote = {
     key: v4(),
@@ -31,13 +36,13 @@ function addNote(type, locationId) {
     isTrashed: false
   };
 
-  // if (type === 'folder') {
-  //   newNote.folder = locationId;
-  // }
+  if (type === 'folder') {
+    newNote.folder = locationId;
+  }
 
-  // if (type === 'tag') {
-  //   newNote.tags.push(locationId);
-  // }
+  if (type === 'tag') {
+    newNote.tags.push(locationId);
+  }
 
   return notes.insert(newNote);
 }
